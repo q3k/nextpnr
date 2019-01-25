a40296154eaf2c30dace045ec1608dca4dd6d717
David Shah <dave@ds0.me>
2019-01-25 18:26:14 +0000

    HeAP: Use for ECP5 as well as iCE40
    Signed-off-by: David Shah <dave@ds0.me>

--- ecp5/arch.cc
+++ ecp5/arch.cc
@@ -23,6 +23,7 @@
 #include <boost/range/adaptor/reversed.hpp>
 #include <cmath>
 #include <cstring>
+#include "placer_heap.h"
 #include "gfx.h"
 #include "globals.h"
 #include "log.h"
@@ -478,7 +479,7 @@ bool Arch::getBudgetOverride(const NetInfo *net_info, const PortRef &sink, delay
 
 // -----------------------------------------------------------------------
 
-bool Arch::place() { return placer1(getCtx(), Placer1Cfg(getCtx())); }
+bool Arch::place() { return placer_heap(getCtx()); }
 
 bool Arch::route()
 {
